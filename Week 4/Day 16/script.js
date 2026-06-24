32cde2638043dbf3c5e0ed11c1b6af3774408d5c
Atharv Pote <atharvpote14@gmail.com>
2026-06-24 18:29:32 +0530

    24/06/26 Week 4

---
+++ Week 4/Day 16/script.js
@@ -0,0 +1,50 @@
+const name = "Atharv";
+const age = 18;
+const isInter = true;
+let course;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof course);
+console.log("5"+ 8);
+console.log("5" == 5);
+console.log("5" === 5);
+
+
+
+const studentName = "Arjun";
+console.log(typeof studentName);
+
+const intershipDays = 60;
+console.log(typeof intershipDays);
+
+const skills = ["HTML","CSS"];
+skills.push("JavaScript");
+console.log(skills);
+
+let middleName = null;
+let nickname;
+console.log(typeof middleName, typeof nickname);
+
+console.log("Day " + 16);
+console.log(16 + 16);
+console.log(typeof null);
+console.log("22" == 22);
+console.log("22" === 22);
+
+const intern = {
+    name: "Atharv",
+    batch: 2028
+};
+
+console.log(typeof intern, intern.name);
+
+if (true) {
+    var leaked = "I'm Outsider"
+};
+console.log(leaked);
+
+const a = "Atharv";
+const b = 2028;
+
+console.log(`${a} graduates in ${b}`);
